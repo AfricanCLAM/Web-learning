774b97fda9757b5531b88e9fe5a818eb92a33a4f
AfricanCLAM <rafir0532@gmail.com>
2024-03-23 01:05:19 +0700

    Adding Try-catch method for Error

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-import {coffeeStock,isCoffeeMachineReady} from './state.js';
+let coffeeStock ,isCoffeeMachineReady;
 
-console.log(coffeeStock);
-console.log(isCoffeeMachineReady);
+  try {
+    const stateModule= await import('./state.js');
+    coffeeStock =stateModule.coffeeStock;
+    isCoffeeMachineReady=stateModule.isCoffeeMachineReady;
+  } catch (importError) {
+    console.log("Error occurred while importing from state.js:", importError);
+  }
+
+  try {
+    console.log(coffeeStock);
+    console.log(isCoffeeMachineReady);
+  } catch (error) {
+    if (error instanceof ReferenceError) {
+    console.log("Error undefined variable used:", error);
+  } else {
+    console.log("Error occurred:",error);
+  }
+}
+
