6e902c26717f59048a370f90a30f96bbf6f6108b
AfricanCLAM <rafir0532@gmail.com>
2024-03-27 20:59:49 +0700

    adding Asynchronous (settimeout)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let coffeeStock ,isCoffeeMachineReady;
 
+//handling error "import module"
   try {
     const stateModule= await import('./state.js');
     coffeeStock =stateModule.coffeeStock;
@@ -8,14 +9,21 @@ let coffeeStock ,isCoffeeMachineReady;
     console.log("Error occurred while importing from state.js:", importError);
   }
 
-  try {
-    console.log(coffeeStock);
-    console.log(isCoffeeMachineReady);
-  } catch (error) {
-    if (error instanceof ReferenceError) {
-    console.log("Error undefined variable used:", error);
-  } else {
-    console.log("Error occurred:",error);
-  }
-}
+//Pembuka
+console.log("selamat datang admin kedai kopi :)");
 
+
+//handling error "mencetak variable import"
+//timeout Asynchronous
+ setTimeout(() => {
+     try {
+     console.log("stok: ",coffeeStock);
+     console.log("Mesin kopi siap: ", isCoffeeMachineReady);
+   } catch (error) {
+     if (error instanceof ReferenceError) {
+     console.log("Error undefined variable used:", error);
+   } else {
+     console.log("Error occurred:",error);
+   }
+ }
+},3000)
